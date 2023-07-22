55a80dae69c8362878177da35dc9d2024b8f0538
Jakub Fronczyk <jakubfronczyk@gmail.com>
2023-07-22 19:32:16 +0200

    change styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 /* eslint-disable react/no-unescaped-entities */
+import Posts from "./components/Posts";
+
 export default function Home() {
     return (
-        <main className="px-6 mx-auto">
+        <main className="px-6 mx-auto max-w-[1200px]">
             <p className="my-12 tex-3xl text-center dark:text-white">
                 Hello, I'm <span className="text-sky-700">Jakub Fronczyk</span>
             </p>
+            <Posts />
         </main>
     );
 }
